cab6603ecc4e72da9be01f8e936306f0a0a57648
talita <polikashina@gmail.com>
2020-10-01 15:54:04 +0200

    deploy

---
+++ root/precache-manifest.0a36df3ad37754c7a59108d23a4f8e38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24191e17825f1f9726de1eae3363a8bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "389683d862aba7db50bf",
+    "url": "/static/css/2.fd554b7d.chunk.css"
+  },
+  {
+    "revision": "e559ea2ff1ec1152a0aa",
+    "url": "/static/css/main.c5c98dc9.chunk.css"
+  },
+  {
+    "revision": "389683d862aba7db50bf",
+    "url": "/static/js/2.ce2ceeba.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.ce2ceeba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e559ea2ff1ec1152a0aa",
+    "url": "/static/js/main.2112b2eb.chunk.js"
+  },
+  {
+    "revision": "ec0bbae65b2d3bd330fa",
+    "url": "/static/js/runtime-main.025b837f.js"
+  },
+  {
+    "revision": "90e2eb44dfe52da5c3ce1bd0069da94e",
+    "url": "/static/media/sunrise.90e2eb44.svg"
+  },
+  {
+    "revision": "9e8ebd38bd9ce630b5d58e996bdcdcc4",
+    "url": "/static/media/sunset.9e8ebd38.svg"
+  }
+]);
