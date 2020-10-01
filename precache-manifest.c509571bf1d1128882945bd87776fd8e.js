ebcdfc0f8d98fb116c5d4c6f82ca152d27f73ef2
talita <polikashina@gmail.com>
2020-10-01 16:45:22 +0200

    Updates

---
+++ precache-manifest.c509571bf1d1128882945bd87776fd8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4788568f003a92f2b6162fa64086843d",
+    "url": "/sunrise-sunset-app/index.html"
+  },
+  {
+    "revision": "389683d862aba7db50bf",
+    "url": "/sunrise-sunset-app/static/css/2.fd554b7d.chunk.css"
+  },
+  {
+    "revision": "9fd9b512f76ea8394d30",
+    "url": "/sunrise-sunset-app/static/css/main.c5c98dc9.chunk.css"
+  },
+  {
+    "revision": "389683d862aba7db50bf",
+    "url": "/sunrise-sunset-app/static/js/2.ce2ceeba.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/sunrise-sunset-app/static/js/2.ce2ceeba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd9b512f76ea8394d30",
+    "url": "/sunrise-sunset-app/static/js/main.3abbb775.chunk.js"
+  },
+  {
+    "revision": "187e98fa226eaaefdd53",
+    "url": "/sunrise-sunset-app/static/js/runtime-main.376fe2ed.js"
+  },
+  {
+    "revision": "90e2eb44dfe52da5c3ce1bd0069da94e",
+    "url": "/sunrise-sunset-app/static/media/sunrise.90e2eb44.svg"
+  },
+  {
+    "revision": "9e8ebd38bd9ce630b5d58e996bdcdcc4",
+    "url": "/sunrise-sunset-app/static/media/sunset.9e8ebd38.svg"
+  }
+]);
